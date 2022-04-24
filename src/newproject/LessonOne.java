e14b8f7c1b59d1588f4d191078a5504e251a7246
BorntoExplore <zay791919@gmail.com>
2022-04-24 12:02:18 +0630

    part one Lesson1

---
+++ src/newproject/LessonOne.java
@@ -0,0 +1,11 @@
+package newproject;
+
+public class LessonOne {
+    public static void main(String[] args) {
+        int a =34+35;
+        System.out.println(a);
+        int b=12;
+        int c=b/2;
+        System.out.println(c);
+    }
+}
